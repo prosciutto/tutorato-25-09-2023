b73618f30554b7074868fed419a3853c02adbbf6
Sapo <frasap97@gmail.com>
2023-09-26 10:47:46 +0200

    modificato codice in main, salutiè una classe a se e calcolatrice ha implementato funzioni nel main per le 4 operazioni principLI

--- src/main/java/it/unisa/studenti/saporito/f/Main.java
+++ src/main/java/it/unisa/studenti/saporito/f/Main.java
@@ -1,7 +1,35 @@
 package it.unisa.studenti.saporito.f;
+import  java.lang.System;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Saluti ciao = new Saluti();
+        Calcolatrice calcolatrice = new Calcolatrice();
+        System.out.println(ciao.Saluti);
+        Scanner sc = new Scanner(System.in);
+        int a=0;
+        int b=0;
+
+        System.out.println("Inserisci un numero");
+        a=sc.nextInt();
+        System.out.println("Inserisci un altro numero");
+        b=sc.nextInt();
+
+        while(a==0 || b==0){
+            System.out.println("bro non lo zero dai");
+            System.out.println("Inserisci un numero");
+            a=sc.nextInt();
+            System.out.println("Inserisci un altro numero");
+            b=sc.nextInt();
+        }
+
+        System.out.println("la somma è :"+calcolatrice.somma(a,b));
+        System.out.println("la differenza è :"+calcolatrice.differenza(a,b));
+        System.out.println("il prodotto è :"+calcolatrice.prodotto(a,b));
+        System.out.println("il quoziente è :"+calcolatrice.divisione(a,b));
+
     }
+
 }
